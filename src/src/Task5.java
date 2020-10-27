858eca774dc88408159b4632e5579beba1ef298e
IDoroftei <irina.doroftei@endava.com>
2020-10-27 22:01:26 +0200

    Task 5 * Changed main class

--- src/src/Task5.java
+++ src/src/Task5.java
@@ -1,6 +1,7 @@
 public class Task5 {
     public static void main(String[] args) {
         System.out.println("Hello this is GitTask");
+        System.out.println("Hello class has been changed");
     }
 }
 
